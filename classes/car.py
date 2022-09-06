ccd3093751ce24335c332360a5790638e4321504
Cammy Smith <cjsarab1991@gmail.com>
2022-09-06 14:22:10 +0100

    Non-DRY code

---
+++ classes/car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self):
+        pass
+
+    def start_engine(self):
+        return "Vrrmmm"
---
+++ classes/motorbike.py
@@ -0,0 +1,6 @@
+class Motorbike:
+    def __init__(self):
+        pass
+
+    def start_engine(self):
+        return "Vrrmmm"
